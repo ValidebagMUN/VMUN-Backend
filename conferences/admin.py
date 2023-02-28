bf43888d5bf1455526b71f3afba1190a50db6d8c
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-02-28 03:41:37 +0300

    feat: register admin pages of Institution, Conference, Committee models

--- conferences/admin.py
+++ conferences/admin.py
@@ -1,3 +1,26 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Committee, Conference, Institution
+
+
+class ConferenceAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "venue", "host", "start", "end")
+    list_filter = ("name", "slug", "venue", "host", "start", "end")
+    search_fields = ("name", "slug", "venue", "host", "start", "end")
+
+
+class CommitteeAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "conference")
+    list_filter = ("name", "slug", "conference")
+    search_fields = ("name", "slug", "conference")
+
+
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "address")
+    list_filter = ("name", "slug", "address")
+    search_fields = ("name", "slug", "address")
+
+
+admin.site.register(Conference, ConferenceAdmin)
+admin.site.register(Committee, CommitteeAdmin)
+admin.site.register(Institution, InstitutionAdmin)
