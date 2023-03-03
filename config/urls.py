c6d88ca5f1da6a2efeeafbda5e19224c02b1b120
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-03-03 12:47:26 +0300

    fix: typo "urlpatterns"

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
-url_patterns = [
+urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
@@ -22,10 +22,10 @@ url_patterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     # Static file serving when using Gunicorn + Uvicorn for local web socket development
-    url_patterns += staticfiles_urlpatterns()
+    urlpatterns += staticfiles_urlpatterns()
 
 # API URLS
-url_patterns += [
+urlpatterns += [
     # API base url
     path("api/", include("config.api_router")),
     # DRF auth token
@@ -41,7 +41,7 @@ url_patterns += [
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
-    url_patterns += [
+    urlpatterns += [
         path(
             "400/",
             default_views.bad_request,
@@ -62,4 +62,4 @@ if settings.DEBUG:
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
-        url_patterns = [path("__debug__/", include(debug_toolbar.urls))] + url_patterns
+        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
